b7da25e74ad3724efffe8a6a89902d45843e2fdf
Shawaf Mohammed <shawaf@source.one>
2021-11-15 14:38:28 +0530

    Added basic connectivity to server

---
+++ src/components/ServerTestComponent.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+
+function ServerTestComponent(){
+    const [message, setMessage] = useState("")
+    async function fetchData () {
+        console.log("fetching Data");
+        const response = await axios.get("http://localhost:7000/");
+        const {msg} = response.data
+        setMessage(msg)
+        console.log(msg)
+    }
+    return (
+        <div>
+            <button onClick={fetchData}>Fetch Data</button>
+            {message && <>
+            <h1>{message}</h1>
+            <button onClick={() => setMessage("")}>Clear</button>
+            </>}
+            
+        </div>
+    )
+}
+
+export default ServerTestComponent;
